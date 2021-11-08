93a9dee76bb36b72427f0a9c2f4ed962d0a33e1f
Maniek1892 <Elino1892@users.noreply.github.com>
2021-11-09 00:07:47 +0100

    Add React file frontend

---
+++ polls_app/urls.py
@@ -0,0 +1,29 @@
+"""polls_app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+<<<<<<< HEAD
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name = 'index.html'))
+=======
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+>>>>>>> b26554a3792eb089e7a7da74a6809cfaea929e98
+]
