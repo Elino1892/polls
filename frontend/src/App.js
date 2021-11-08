2199bf4460c7dbcee6599bd34f9aca2b275c8d63
Maniek1892 <Elino1892@users.noreply.github.com>
2021-11-09 00:21:06 +0100

    Add React file frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactttasdasd
+          Learn React
         </a>
       </header>
     </div>
